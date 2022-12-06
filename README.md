df5e579a8fc71112d315d8db066aa7749e493d65
Mateusz Konopka <mkonopkadev@gmail.com>
2022-12-06 21:32:58 +0100

    mvp ready

---
+++ README.md
@@ -0,0 +1,8 @@
+# WeatherApp
+
+Weather forecast Angular app.
+- Angular and REST API used.
+- Searching functionality implemented.
+
+# Snapshots
+ <img src="https://i.ibb.co/KG01g1g/weatherapp-snapshot.jpg" width="400" title="hover text">
---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './models/weather.model';
+import { WeatherService } from './services/weather.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  constructor(private weatherService: WeatherService) {}
+
+  cityName: string = "Wroclaw";
+  weatherData?: WeatherData;
+
+  ngOnInit(): void {
+    this.onSubmit();
+  }
+
+  private getWeatherData(cityName: string) {
+    this.weatherService.getWeatherData(cityName)
+      .subscribe({
+        next: (response) => {
+          this.weatherData = response;
+          console.log(response);
+        }
+      });
+  }
+
+  onSubmit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+}
